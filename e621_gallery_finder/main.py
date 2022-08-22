52aaeb1e27d126d4081bef49aee32f75a188bf28
Deer-Spangle <deer@spangle.org.uk>
2022-08-22 09:28:23 +0100

    Making find_matching_sources() method return list of sources

--- e621_gallery_finder/main.py
+++ e621_gallery_finder/main.py
@@ -1,5 +1,5 @@
 import csv
-from typing import List, Dict
+from typing import List, Dict, Optional
 
 import tqdm
 import requests
@@ -10,6 +10,29 @@ from e621_gallery_finder.source_checks import FAUserLink, FADirectLink, TwitterG
     FixableSourceMatch
 from e621_source_cleanup.checks.base import BaseCheck
 from e621_source_cleanup.main import setup_max_int, fetch_db_dump_path, csv_line_count
+
+
+def post_to_url(site_id: str, post_id: str, username: Optional[str] = None) -> str:
+    return {
+        "e621": f"https://e621.net/posts/{post_id}/",
+        "fa": f"https://www.furaffinity.net/view/{post_id}/",
+        "twitter": f"https://twitter.com/{username or 'user'}/status/{post_id}"
+    }[site_id]
+
+
+def replace_prefix(source: str, old_prefix: str, new_prefix: str) -> str:
+    if source.startswith(old_prefix):
+        return new_prefix + source[len(old_prefix):]
+    return source
+
+
+def clean_direct_link(direct_link: Optional[str]) -> Optional[str]:
+    if direct_link is None:
+        return None
+    direct_link = replace_prefix(direct_link, "http://", "https://")
+    direct_link = replace_prefix(direct_link, "https://d.facdn.net/", "https://d.furaffinity.net/")
+    direct_link = replace_prefix(direct_link, "https://d2.facdn.net/", "https://d.furaffinity.net/")
+    return direct_link
 
 
 def scan_csv(csv_path: str, checks: List[BaseCheck]) -> Dict[str, List[FixableSourceMatch]]:
@@ -74,13 +97,13 @@ class PostFixer:
         self._hash_id_priority = id_priority
         return id_priority
 
-    def find_matching_source(self, post_id: str, post_issues: PostIssues) -> None:
+    def find_matching_source(self, post_id: str, post_issues: PostIssues) -> List[str]:
         faxdb_post_data = requests.get(
             f"https://faexportdb.spangle.org.uk/api/view/submissions/e621/{post_id}.json"
         ).json()
         post_hashes = faxdb_post_data["data"]["submission_data"]["files"][0]["file_hashes"]
-        found = False
         remaining_match_infos = post_issues.unique_match_info()
+        new_sources = []
         for hash_id in self.hash_id_priority:
             value = next(iter(
                 [file_hash["hash_value"] for file_hash in post_hashes if file_hash["algo_id"] == hash_id]
@@ -94,25 +117,34 @@ class PostFixer:
                     "hash_value": value,
                 }
             ).json()
+            e6_link = post_to_url("e621", post_id)
             for snapshot in matching_results["results"]:
                 if snapshot["website_id"] == "e621":
                     if snapshot["site_submission_id"] == post_id:
                         # Tautologically true self-match
                         continue
-                    print(f"Another post ({snapshot['site_submission_id']}) on e621 matches hash of post: {post_id}")
+                    other_e6_link = post_to_url("e621", snapshot["site_submission_id"])
+                    print(f"Another post ({other_e6_link}) on e621 matches hash of post: {e6_link}")
                     continue
                 for match_info in remaining_match_infos[:]:
                     if match_info.might_match_snapshot(snapshot):
                         remaining_match_infos.remove(match_info)
-                        print(f"Found a potential source for post: {post_id}, Site: {snapshot['website_id']} & ID: {snapshot['site_submission_id']}")
+                        submission_url = post_to_url(snapshot['website_id'], snapshot['site_submission_id'], match_info.site_user_id)
+                        new_sources.append(submission_url)
+                        direct_link = clean_direct_link(snapshot["submission_data"]["files"][0]["file_url"])
+                        if direct_link is not None:
+                            new_sources.append(direct_link)
+                        print(f"Found a potential source for post: {e6_link}, {submission_url}")
         if remaining_match_infos:
-            print(f"Can't find any matches for post {post_id}")
-        return None
+            print(f"Can't find any matches for post {e6_link}")
+        return new_sources
 
     def fix_sources(self, match_dict: Dict[str, List[FixableSourceMatch]]) -> None:
         for post_id, matches in match_dict.items():
             post_issues = PostIssues(matches)
-            self.find_matching_source(post_id, post_issues)
+            new_sources = self.find_matching_source(post_id, post_issues)
+            if new_sources:
+                self.api.add_new_sources(post_id, new_sources)
 
 
 if __name__ == "__main__":
