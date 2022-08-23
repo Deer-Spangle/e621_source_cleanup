49fdd86ce1b6f9072e5d7724e855f77616e63140
Deer-Spangle <deer@spangle.org.uk>
2022-08-23 09:17:10 +0100

    Adding source_id column to the database

--- e621_gallery_finder/db_schema.sql
+++ e621_gallery_finder/db_schema.sql
@@ -9,6 +9,9 @@ CREATE UNIQUE INDEX IF NOT EXISTS post_status_post_id
     ON post_status (post_id);
 
 CREATE TABLE IF NOT EXISTS post_new_sources (
+    source_id integer
+        constraint table_name_pk
+            primary key autoincrement,
     post_id str not null,
     submission_link str,
     direct_link str,
